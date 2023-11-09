8616c827360491a2e7351d7bb206c8f5ec6d7803
Juan Caicedo <jesteban.caicedo@udea.edu.co>
2023-11-09 02:49:57 -0500

    Juego finalizado

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <sstream>
+#include <chrono>
+#include <ctime>
 #include "espacio.h"
 using namespace std;
 
@@ -8,55 +12,88 @@ string movusuario(string *);
 void imprimirjuego(Espacio **);
 void reiniciaropciones(string *, int *);
 void jugada(Espacio ** , string *, int *, string, char);
-bool finjuego(Espacio **, string *);
+bool finopciones(string *);
+void finjuego(Espacio **);
+void imphistorial();
 
 int filas = 8, columnas = 8;
 
 int main()
 {
-    Espacio** matriz = new Espacio*[filas];
+    cout<<"Bienvenido a eothello"<<endl;
+    bool menu= true;
+    short int mov=0;
+    while(menu){
+        cout<<"Ingrese:"<<endl<<"1-Para jugar"<<endl<<"2-Para ver historial"<<endl<<"3-Para salir: ";
+        cin>>mov;
+        while(mov<1 || mov>3){
+            cout<<"Ingrese un movimiento valido: ";
+            cin>>mov;
+        }
+        if (mov==1){
+            Espacio** matriz = new Espacio*[filas];
 
-    for (int i = 0; i < filas; i++) {
-        matriz[i] = new Espacio[columnas];
-    }
-    char signo2='X', signo1='O';
-    matriz[3][3].setSigno(signo2);
-    matriz[3][4].setSigno(signo1);
-    matriz[4][3].setSigno(signo1);
-    matriz[4][4].setSigno(signo2);
-    bool juego=true, turno=true;
-    string mov;
-    string *opciones = new string[30];
-    int *copciones = new int[30];
-    opcionesjuego(matriz,signo1, signo2,opciones,copciones);
-    while(juego){
-        imprimirjuego(matriz);
-        if(turno){
-            cout<<"Turno de O"<<endl;
-            mov=movusuario(opciones);
-            jugada(matriz, opciones, copciones, mov, signo1);
-            turno=false;
-            reiniciaropciones(opciones,copciones);
-            opcionesjuego(matriz,signo2, signo1,opciones,copciones);
+            for (int i = 0; i < filas; i++) {
+                matriz[i] = new Espacio[columnas];
+            }
+            char signo2='X', signo1='O';
+            matriz[3][3].setSigno(signo2);
+            matriz[3][4].setSigno(signo1);
+            matriz[4][3].setSigno(signo1);
+            matriz[4][4].setSigno(signo2);
+            bool juego=true, turno=true, auxturno=true;
+            string mov;
+            string *opciones = new string[30];
+            int *copciones = new int[30];
+            opcionesjuego(matriz,signo1, signo2,opciones,copciones);
+            while(juego){
+                imprimirjuego(matriz);
+                if(turno){
+                    cout<<"Turno de O"<<endl;
+                    mov=movusuario(opciones);
+                    jugada(matriz, opciones, copciones, mov, signo1);
+                    turno=false;
+                    reiniciaropciones(opciones,copciones);
+                    opcionesjuego(matriz,signo2, signo1,opciones,copciones);
+                    auxturno=finopciones(opciones);
+                    if(auxturno==false){
+                        turno=true;
+                        reiniciaropciones(opciones,copciones);
+                        opcionesjuego(matriz,signo1, signo2,opciones,copciones);
+                    }
+                }
+                else{
+                    cout<<"Turno de X"<<endl;
+                    mov=movusuario(opciones);
+                    jugada(matriz, opciones, copciones, mov, signo2);
+                    turno=true;
+                    reiniciaropciones(opciones,copciones);
+                    opcionesjuego(matriz,signo1, signo2,opciones,copciones);
+                    auxturno=finopciones(opciones);
+                    if(auxturno==false){
+                        turno=false;
+                        reiniciaropciones(opciones,copciones);
+                        opcionesjuego(matriz,signo2, signo1,opciones,copciones);
+                    }
+                }
+                juego=finopciones(opciones);
+            }
+            finjuego(matriz);
+
+            for (int i = 0; i < filas; i++) {
+                delete[] matriz[i];
+            }
+            delete[] matriz;
+            delete[] opciones;
+            delete[] copciones;
+        }
+        else if(mov==2){
+            imphistorial();
         }
         else{
-            cout<<"Turno de X"<<endl;
-            mov=movusuario(opciones);
-            jugada(matriz, opciones, copciones, mov, signo2);
-            turno=true;
-            reiniciaropciones(opciones,copciones);
-            opcionesjuego(matriz,signo1, signo2,opciones,copciones);
+            menu=false;
         }
-        juego=finjuego(matriz,opciones);
     }
-
-    for (int i = 0; i < filas; i++) {
-        delete[] matriz[i];
-    }
-    delete[] matriz;
-    delete[] opciones;
-    delete[] copciones;
-
     return 0;
 }
 
@@ -381,33 +418,75 @@ void jugada(Espacio **matriz , string *opciones, int *copciones, string mov, cha
     }
 }
 
-bool finjuego(Espacio **matriz, string *opciones)
+bool finopciones(string *opciones)
 {
-    int X=0, O=0;
     bool juego=true;
     if(opciones[0]==""){
-        imprimirjuego(matriz);
         juego=false;
-        for(int i=0; i<filas;i++){
-            for(int j=0; j<columnas;j++){
-                if(matriz[i][j].getSigno()=='X'){
-                    X+=1;
-                }
-                else if(matriz[i][j].getSigno()=='O'){
-                    O+=1;
-                }
-            }
-        }
-        cout<<"El juego ha finalizado"<<endl;
-        if(X>O){
-            cout<<"El ganador es X"<<endl;
-        }
-        else if(X<O){
-            cout<<"El ganador es O"<<endl;
-        }
-        else{
-            cout<<"Es un empate"<<endl;
-        }
     }
     return juego;
+}
+
+void finjuego(Espacio **matriz)
+{
+    string jugx, jugo, juego;
+    int x=0, o=0;
+    imprimirjuego(matriz);
+    for(int i=0; i<filas; i++){
+        for(int j=0; j<columnas; j++){
+            if(matriz[i][j].getSigno()=='X'){
+                x+=1;
+            }
+            else if(matriz[i][j].getSigno()=='O'){
+                o+=1;
+            }
+        }
+    }
+    if(x<o){
+        cout<<"El ganador es el jugador O"<<endl;
+        juego="-pierde con-";
+    }
+    else if(x>o){
+        cout<<"El ganador es el jugador X"<<endl;
+        juego="-gana a-";
+    }
+    else{
+        cout<<"Ha sido un empate"<<endl;
+        juego="-empata con-";
+    }
+    auto now = chrono::system_clock::now();
+    time_t tiempo_actual = chrono::system_clock::to_time_t(now);
+    tm local_time = *localtime(&tiempo_actual);
+    char buffer[80];
+    strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", &local_time);
+    cout<<"Ingrese el nombre del jugador X: ";
+    cin>>jugx;
+    cout<<"Ingrese el nombre del jugador O: ";
+    cin>>jugo;
+    ofstream archi;
+    archi.open("Partidas.txt", ios::app);
+    archi<<jugx;
+    archi<<'[';
+    archi<<x;
+    archi<<']';
+    archi<<juego;
+    archi<<jugo;
+    archi<<'[';
+    archi<<o;
+    archi<<']';
+    archi<<'-';
+    archi<<buffer;
+    archi<<'\n';
+    archi.close();
+}
+
+void imphistorial()
+{
+    string linea;
+    ifstream arch;
+    arch.open("Partidas.txt");
+    while(arch.good()){
+        getline(arch,linea);
+        cout<<linea<<endl;
+    }
 }
